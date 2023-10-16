0daa63dbc57b195a1d3bd7f16b615107e88e4227
Angel <97441137+Soul1227@users.noreply.github.com>
2023-10-16 14:38:30 +0200

    methods added: -add -remove -view -calculate Junit completed

--- Application/src/ShoppingCart.java
+++ Application/src/ShoppingCart.java
@@ -1,18 +1,47 @@
+import java.util.LinkedList;
+
 public class ShoppingCart {
-    private Product[] products;
+    private LinkedList<Product> products = new LinkedList<>();
 
     public ShoppingCart() {
     }
 
-    public ShoppingCart(Product[] products) {
+    public ShoppingCart(LinkedList<Product> products) {
         this.products = products;
     }
 
-    public Product[] getProducts() {
+    public void add(Product product) {
+        products.add(product);
+    }
+
+    public void remove(int position) {
+        try {
+            products.remove(position);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("there is no item in that position");
+        }
+    }
+
+    public void view() {
+        for (int x = 0; x < products.size(); x++) {
+            System.out.println("Item position: " + x + "\n " + products.get(x).toString());
+        }
+    }
+
+    public double calculate() {
+        double finalPrice = 0;
+        for (Product product : products
+        ) {
+            finalPrice += product.getPrice();
+        }
+        return finalPrice;
+    }
+
+    public LinkedList<Product> getProducts() {
         return products;
     }
 
-    public void setProducts(Product[] products) {
+    public void setProducts(LinkedList<Product> products) {
         this.products = products;
     }
 }
